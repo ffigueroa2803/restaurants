03dbbc793625204d8c5e6cf4ccd4cfa2a50d9160
ffigueroa2803 <ffigueroa2803@gmail.com>
2021-03-14 15:53:01 -0500

    Navigate to login page

--- app/navigations/AccountStack.js
+++ app/navigations/AccountStack.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 
 import Account from '../screens/account/Account'
+import Login from '../screens/account/Login'
 
 const Stack = createStackNavigator()
 
@@ -12,6 +13,11 @@ const AccountStack = () => {
                 name="account-stack"
                 component={Account}
                 options={{ title:"Cuenta" }}
+            />
+            <Stack.Screen 
+                name="login-stack"
+                component={Login}
+                options={{ title:"Iniciar sesión" }}
             />
         </Stack.Navigator>
     )
--- app/screens/account/UserGuest.js
+++ app/screens/account/UserGuest.js
@@ -1,14 +1,61 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, ScrollView, StyleSheet, Text } from 'react-native'
+import { useNavigation} from '@react-navigation/native'
+
+import { Button } from 'react-native-elements'
 
 const UserGuest = () => {
+
+    const navigation = useNavigation()
+
     return (
-        <View>
-            <Text>UserGuest.......</Text>
-        </View>
+        <ScrollView
+            centerContent={true}
+            style={styles.viewBody}
+        >
+            <Image 
+                source={require("../../assets/restaurant-logo.png")} 
+                resizeMode="contain"
+                style={styles.image}
+            />
+            <Text style={styles.title}>Consulta tu perfil en Restaurants</Text>
+            <Text style={styles.description}>
+                ¿Cómo describirías tu mejor restaurante? Busca y visualiza los mejores restaurantes de una forma sencilla, vota cuál te ha     gustado más y comenta cómo a sido tu experiencia.
+            </Text>
+            <Button
+                buttonStyle={styles.button}
+                title="Ver tu perfil"
+                onPress={() => navigation.navigate("login-stack")}
+            />
+        </ScrollView>
     )
 }
 
 export default UserGuest
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    viewBody: {
+        marginHorizontal: 30
+    },
+    image: {
+        height: 300,
+        width: "100%",
+        marginBottom: 10,
+        alignContent: "center",
+        alignItems: "center"
+    },
+    title: {
+        fontWeight: "bold",
+        fontSize: 19,
+        marginVertical: 10,
+        textAlign: "center"
+    },
+    description: {
+        textAlign: "justify",
+        marginBottom: 20,
+        color: "#a65273"
+    },
+    button: {
+        backgroundColor: "#442484"
+    }
+})
